03114727ac4254fae82f5dcd12b458218ba997f4
cpfeiffer325 <cpfeiffer325@gmail.com>
2020-01-11 14:27:24 -0700

    refactor to call components into page layout

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,34 +1,10 @@
 import React from 'react'
-import {TaskList} from '../../components/TaskList'
-import '../../index.css'
-import 'antd/dist/antd.css'
-import { Layout, Row, Col } from 'antd'
-
-const { Header, Footer, Content } = Layout
+import { Layout } from '../../components/Layout'
 
 export const Dashboard = () => {
   return (
     <div>
-      <Layout>
-        <Header>Header</Header>
-        <Content           
-          style={{
-            background: '#fff',
-            padding: 24,
-            paddingLeft: 60,
-            paddingRight: 60,
-            margin: 0,
-            minHeight: 280,
-          }}>
-          <TaskList />
-          <Row>
-            <Col span={8}>New Project +</Col>
-            <Col span={8}>Meta Labs Project</Col>
-            <Col span={8}>Other Project</Col>
-          </Row>
-        </Content>
-        <Footer>Footer</Footer>
-      </Layout>
+      <Layout />
     </div>
   )
 }
