b5e37017741cd8a28f0fb88714f18f0449ed2743
cpfeiffer325 <cpfeiffer325@gmail.com>
2020-01-11 14:22:32 -0700

    update Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import { Header } from '../Header'
+import { Layout as LayoutExt } from '../../external_components'
+import { TaskList } from '../../components/TaskList'
+import { ProjectsPanel } from '../../components/ProjectsPanel'
+
+const { Content } = LayoutExt
 
 export const Layout = (props) => {
   return (
     <div>
-      <Header />
-      {props.children}
+      <LayoutExt>
+        <Header />
+        {props.children}
+        <Content           
+          style={{
+            background: '#fff',
+            padding: 24,
+            paddingLeft: 60,
+            paddingRight: 60,
+            margin: 0,
+            minHeight: 280,
+          }}>
+          <TaskList />
+          <div>Create Toggle Component: My Project / All Projects</div>
+          <ProjectsPanel />
+        </Content>
+      </LayoutExt>
     </div>
   )
 }
