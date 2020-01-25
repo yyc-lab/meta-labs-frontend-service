9992dc86f54bf62a0e27b42baabcab4a8e17b92e
Andrew Kim <andrew.kim@neofinancial.com>
2020-01-25 12:17:34 -0700

    creating basic api hook

--- src/utils/data/dataGet.js
+++ src/utils/data/dataGet.js
@@ -8,6 +8,7 @@ import { URL } from '../../env';
 * @parameter: string endPoint, string
 * @return Promise <React DOM/> 
 */
+
 export const getCall = (endPoint, newRoute = '') => {
   return axios.get(`${URL}${endPoint}`)
   .then(res =>  {
@@ -24,3 +25,35 @@ export const getCall = (endPoint, newRoute = '') => {
     return <Redirect to='/' />
   });
 }
+
+const CRUD_TO_METHOD = {
+  GET: 'get',
+  UPDATE: 'put',
+  CREATE: 'create',
+  DELETE: 'delete'
+}
+
+const defaultApiCall = ({ crudType, resource, body, filter }) => {
+  const inputCrudType = crudType.split('_')[0];
+  const method = CRUD_TO_METHOD[inputCrudType];
+  const idRequiredMethods = ['PUT', 'DELETE', 'GET_ONE'];
+
+  let url = `/${resource}`;
+
+  if (idRequiredMethods.includes(crudType)) {
+    if (!body.id) throw new Error('id not provided');
+    url += `/${body.id}`
+  }
+
+  if (crudType === 'GET_MANY' && filter) {
+    // url += `?filter=${filter}`
+  }  
+  
+  return {
+    url,
+    method,
+    body,
+  }
+}
+
+export default defaultApiCall
