c3aa534c7045fa8063b1983fad489066b63a9d08
cpfeiffer325 <cpfeiffer325@gmail.com>
2020-01-25 14:35:40 -0700

    create base new project form

--- src/pages/NewProject/NewProject.js
+++ src/pages/NewProject/NewProject.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import 'antd/dist/antd.css';
+import { Form, Input, Button } from 'antd';
+
 export const NewProject = () => {
   return (
     <div>
-      New Project Page
+      <Form>
+        <Form.Item label="Project Name">
+          <Input placeholder="input placeholder" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary">Submit</Button>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
